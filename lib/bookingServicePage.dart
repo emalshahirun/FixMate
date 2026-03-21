b6f75754b4be31a2d86368fd459587de9147bbd6
Sahas  Perera <gslakviruperera@gmail.com>
2026-03-22 03:03:20 +0530

    bookingServicePage.dart added the add method to save the bookings and error handling

--- lib/bookingServicePage.dart
+++ lib/bookingServicePage.dart
@@ -10,4 +10,19 @@ class BookingService {
     required String serviceType,
     required String date,
     required String duration,
-  }) async {}
+  }) async {
+    try {
+      await bookings.add({
+        'customerName': customerName,
+        'serviceType': serviceType,
+        'date': date,
+        'duration': duration,
+        'status': 'confirmed',
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+    } catch (e) {
+      print("Error saving booking: $e");
+    }
+  }
+}
+
