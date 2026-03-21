dc65849eaee0678a27118b82fe044f20f2c4bea3
Sahas  Perera <gslakviruperera@gmail.com>
2026-03-22 02:55:25 +0530

    added the saveBooking method

---
+++ lib/bookingServicePage.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class BookingService {
+  final CollectionReference bookings =
+      FirebaseFirestore.instance.collection('bookings');
+
+
+    Future<void> saveBooking({
+    required String customerName,
+    required String serviceType,
+    required String date,
+    required String duration,
+  }) async {}
