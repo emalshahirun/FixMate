c55923b5d0683cfd79eec8197598752c13f6c3fd
Sahas  Perera <gslakviruperera@gmail.com>
2026-03-21 18:20:26 +0530

    bookingSuccessfulPage.dart the code added the confirmation message

--- lib/bookingSuccessfulPage.dart
+++ lib/bookingSuccessfulPage.dart
@@ -13,3 +13,11 @@ class BookingSuccessfulPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const CircleAvatar(radius: 40, backgroundColor: Colors.green, child: Icon(Icons.check, color: Colors.white, size: 50)),
+            const SizedBox(height: 20),
+            const Text("Booking Successful !", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.green)),
+            const SizedBox(height: 15),
+            const Text(
+              "Dear Harry Styles, you have successfully scheduled booking of Plumber for the upcoming date 12 Dec. Our service provider will contact you soon.",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.grey),
+            ),
