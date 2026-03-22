bfb1fc8b07c70005106d761cce6eb840058c212c
Anuhas <silentkilleranuhas@email.com>
2026-03-22 17:28:30 +0530

    Privacy Policy

--- lib/user.dart
+++ lib/user.dart
@@ -34,3 +34,18 @@ appBar: AppBar(
   backgroundColor: Colors.white,
   foregroundColor: Colors.black,
 ),
+// lib/pages/privacy_policy_page.dart
+import 'package:flutter/material.dart';
+import '../widgets/settings_scaffold.dart';
+
+class PrivacyPolicyPage extends StatelessWidget {
+  const PrivacyPolicyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SettingsScaffold(
+      title: "Privacy Policy",
+      body: SizedBox(),
+    );
+  }
+}
