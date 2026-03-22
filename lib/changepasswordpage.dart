6a6806f4a68630be474668fecc04089c0c0f2621
ThilinaDiss94 <dissanyakedrt94@gmail.com>
2026-03-22 17:27:09 +0530

    Create ChangePasswordPage structure

---
+++ lib/changepasswordpage.dart
@@ -0,0 +1,11 @@
+class ChangePasswordPage extends StatelessWidget {
+  const ChangePasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SettingsScaffold(
+      title: "Change Password",
+      body: Container(),
+    );
+  }
+}
