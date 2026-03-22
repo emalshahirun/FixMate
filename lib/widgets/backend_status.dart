b0e78bfaeed65a8464d50f7b21f285337903fe03
Abhisheka0025 <iisuri.20231401@iit.ac.lk>
2026-03-23 05:05:49 +0530

    Created BackendStatus widget structure

---
+++ lib/widgets/backend_status.dart
@@ -0,0 +1,13 @@
+class BackendStatus extends StatefulWidget {
+  const BackendStatus({super.key});
+
+  @override
+  State<BackendStatus> createState() => _BackendStatusState();
+}
+
+class _BackendStatusState extends State<BackendStatus> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
